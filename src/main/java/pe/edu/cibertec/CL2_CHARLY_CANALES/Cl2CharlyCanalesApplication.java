fabc947d0d834f04cc954b2a484adc8b6e1d5576
ccanales2023 <orion.1781@gmail.com>
2023-07-17 16:33:51 -0500

    CL2_CANALES_CHARLY

---
+++ src/main/java/pe/edu/cibertec/CL2_CHARLY_CANALES/Cl2CharlyCanalesApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.CL2_CHARLY_CANALES;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cl2CharlyCanalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cl2CharlyCanalesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/CL2_CHARLY_CANALES/Cl2CharlyCanalesApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.CL2_CHARLY_CANALES;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cl2CharlyCanalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
